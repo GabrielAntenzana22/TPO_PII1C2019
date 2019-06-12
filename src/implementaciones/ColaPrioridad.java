116d33e13c0b81c32da4aa5810d1c1c3c28d05eb
GabrielAntenzana22 <51725403+GabrielAntenzana22@users.noreply.github.com>
2019-06-11 21:25:53 -0300

    Implementacion ColaPrioridad
    11/06/2019

---
+++ src/implementaciones/ColaPrioridad.java
@@ -0,0 +1,54 @@
+package implementaciones;
+import tdas.ColaPrioridadTDA;
+
+public class ColaPrioridad implements ColaPrioridadTDA{
+	
+	String [] elementos;
+	String [] prioridades;
+	int indice;
+
+	@Override
+	public void inicializar() {
+		indice = 0;
+		elementos = new String [100];
+		prioridades = new String [100];
+		
+	}
+
+	@Override
+	public void acolar(String valor, String prioridad) {
+		// desplaza a derecha los elementos de la cola mientras
+		// estos tengan mayor o igual prioridad que la de x
+		int j;
+		for (j = indice ; j>0 && Integer.valueOf(prioridades[j -1]) >= Integer.valueOf(prioridad); j-- ){
+			elementos[j] = elementos[j -1];
+			prioridades[j] = prioridades[j -1];
+		}
+		
+		elementos[j] = valor;
+		prioridades[j] = prioridad;
+		indice++;
+		
+	}
+
+	@Override
+	public void dasacolar() {
+		indice --;		
+	}
+
+	@Override
+	public String primero() {
+		return elementos[indice -1];
+	}
+
+	@Override
+	public String prioridad() {
+		return prioridades[indice -1];
+	}
+
+	@Override
+	public boolean colaVacia() {
+		return ( indice == 0);
+	}
+
+}
