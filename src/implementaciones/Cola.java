a989244cd1e7c9c552740f221a4ed4e88c1def55
GabrielAntenzana22 <51725403+GabrielAntenzana22@users.noreply.github.com>
2019-06-11 21:11:14 -0300

    Implementacion Cola
    11/06/2019

---
+++ src/implementaciones/Cola.java
@@ -0,0 +1,34 @@
+package implementaciones;
+import tdas.ColaTDA;
+
+public class Cola implements ColaTDA {
+	String [] a;
+	int indice;
+	
+	@Override
+	public void inicilizar() {
+		a = new String [100];
+		indice = 0;
+		
+	}
+	@Override
+	public void acolar(String valor) {
+		for ( int i = indice -1; i >=0; i--)
+			a[i+1] = a[i];
+		a[0] = valor;
+		indice++;
+		
+	}
+	@Override
+	public void desacolar() {
+		indice --;
+	}
+	@Override
+	public String primero() {
+		return a[indice -1];
+	}
+	@Override
+	public boolean colaVacia() {
+		return ( indice == 0);
+	}
+}
